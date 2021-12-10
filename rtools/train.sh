74fa13a28a5143fbda20ae57850532f8337c9775
yanglinGEM <1418086279@qq.com>
2021-12-10 10:34:40 +0800

    .

--- rtools/train.sh
+++ rtools/train.sh
@@ -10,3 +10,5 @@ export CUDA_VISIBLE_DEVICES=6
 /home/YangLin/anaconda3/envs/r3det/bin/python tools/train.py configs/r3det/r3det_r50_fpn_2x_20211130.py
 export CUDA_VISIBLE_DEVICES=5
 /home/YangLin/anaconda3/envs/r3det/bin/python tools/train.py configs/r3det/r3det_r50_fpn_2x_20211129_2.0.py
+export CUDA_VISIBLE_DEVICES=6 
+/home/YangLin/anaconda3/envs/r3det/bin/python tools/train.py configs/r3det/r3det_re50_fpn_2x.py
